1a40587f0e7215f0661f3b1ee65ba754fa94da69
sberger94 <sberger94@gmail.com>
2022-04-05 19:53:59 -0700

    updated flights controller

--- controllers/flights.js
+++ controllers/flights.js
@@ -33,8 +33,6 @@ function show(req, res){
     Flight.findById(req.params.id, function(err, flight){
         Ticket.find({flight: flight._id}, function(err, tickets){
             res.render('flights/show', { title: 'Flight Detail', flight, tickets});
-    
-    
         });
     });
 }
